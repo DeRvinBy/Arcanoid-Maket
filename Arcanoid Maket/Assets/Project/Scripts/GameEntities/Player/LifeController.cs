994c881b8fee7da09ac79c6ea15f4acdd70b3b14
Ivashin Daniil <gpanda37@gmail.com>
2021-07-12 20:24:41 +0300

    Player life refactoring

--- Arcanoid Maket/Assets/Project/Scripts/GameEntities/Player/LifeController.cs
+++ Arcanoid Maket/Assets/Project/Scripts/GameEntities/Player/LifeController.cs
@@ -16,28 +16,25 @@ namespace Project.Scripts.GameEntities.Player
         [SerializeField]
         private LifeUI _lifeUI;
 
-        private LifeModel _model;
+        private int _lifeCount;
         
         public override void Initialize()
         {
-            _model = new LifeModel();
-            _lifeUI.Initialize(_settings);
-            
             EventBus.Subscribe(this);
         }
 
         public void OnStartGame()
         {
-            _model.SetLifeCount(_settings.StartLifeCount);
-            _lifeUI.SetLifeCountInUI(_model.LifeCount);
+            _lifeCount = _settings.StartLifeCount;
+            _lifeUI.SetLifeCountInUI(_lifeCount);
         }
         
         public void OnPlayerBallsEnded()
         {
-            _model.ReduceLifeByOne();
-            _lifeUI.SetLifeCountInUI(_model.LifeCount);
+            _lifeCount--;
+            _lifeUI.SetLifeCountInUI(_lifeCount);
             
-            if (_model.LifeCount <= 0)
+            if (_lifeCount <= 0)
             {
                 EventBus.RaiseEvent<IEndGameHandler>(a => a.OnLoseGame());
             }
