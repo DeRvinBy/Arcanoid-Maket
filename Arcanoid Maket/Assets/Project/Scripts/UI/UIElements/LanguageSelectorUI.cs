127cc6302e388d2160b57e7dcbda8d883639bce8
Ivashin Daniil <gpanda37@gmail.com>
2021-07-16 23:58:31 +0300

    Menu
    Created menu ui elements

---
+++ Arcanoid Maket/Assets/Project/Scripts/UI/UIElements/LanguageSelectorUI.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Project.Scripts.UI.UIElements
+{
+    public class LanguageSelectorUI : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Dropdown _selector;
+        
+        
+    }
+}
